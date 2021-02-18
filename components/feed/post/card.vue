01efdb99d13f0b7e5fc7bfffe55a083aafd3bd1f
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-02-18 17:18:35 +0000

    feat: implementing feature card

--- components/feed/post/card.vue
+++ components/feed/post/card.vue
@@ -2,18 +2,19 @@
   <div
     class="md:flex shadow-lg mx-6 md:mx-auto my-40 max-w-lg md:max-w-2xl h-64"
   >
-    <img
+    <card-image />
+    <!--    <img
       class="h-full w-full md:w-1/3 object-cover rounded-lg rounded-r-none pb-5/6"
       src="https://ik.imagekit.io/q5edmtudmz/FB_IMG_15658659197157667_wOd8n5yFyXI.jpg"
       alt="bag"
-    />
+    />-->
     <div class="w-full md:w-2/3 px-4 py-4 bg-white rounded-lg">
       <div class="flex items-center">
-        <h2 class="text-xl text-gray-800 font-medium mr-auto">
-          Your Travel Buddy
-        </h2>
+        <span class="text-sm text-green-800 font-medium mr-auto">
+          How to use Git version control
+        </span>
       </div>
-      <p class="text-sm text-gray-700 mt-4">
+      <p class="text-xs text-gray-700 mt-4">
         Lorem, ipsum dolor sit amet consectetur Amet veritatis ipsam reiciendis
         numquam tempore commodi ipsa suscipit laboriosam, sit earum at sequ
         adipisicing elit. Amet veritatis ipsam reiciendis numquam tempore
---
+++ components/feed/post/microbes/CardImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex-shrink-0 flex items-center">
+    <div class="block lg:hidden h-300 w-300">
+      <cld-image
+        public-id="post_feature/git-tutorials.svg"
+        height="300"
+        width="300"
+        crop="scale"
+        fetch-format="auto"
+        quality="auto"
+        alt="Geek.I.Am"
+        title="Geek.I.Am software developer community"
+      />
+    </div>
+    <div class="hidden lg:block h-210 w-210">
+      <cld-image
+        public-id="post_feature/git-tutorials.svg"
+        height="210"
+        width="210"
+        alt="Geek.I.Am"
+        title="Geek.I.Am software developer community"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardImage',
+}
+</script>
